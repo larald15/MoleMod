b815ed613a56cf33ba380985b97e967699f90ef9
larald15 <larald15@htlkaindorf.at>
2018-10-08 16:01:03 +0200

    Initial Commit

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -25,7 +25,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 }

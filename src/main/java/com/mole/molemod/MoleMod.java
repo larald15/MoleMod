8d0c21cf755efb52aba90f3df3294e54f78c432d
larald15 <larald15@htlkaindorf.at>
2018-10-08 20:11:40 +0200

    Initial Commit

---
+++ src/main/java/com/mole/molemod/MoleMod.java
@@ -0,0 +1,25 @@
+package com.mole.molemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = com.mole.molemod.MoleMod.MODID,
+        name = com.mole.molemod.MoleMod.NAME,
+        version = com.mole.molemod.MoleMod.VERSION)
+public class MoleMod {
+    public static final String MODID = "molemod";
+    public static final String NAME = "Mole Mod";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}

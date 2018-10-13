b0a0af975942f628c667bb951ba65b355843de0c
larald15 <larald15@htlkaindorf.at>
2018-10-14 00:16:09 +0200

    Initial Commit

--- src/main/java/com/mod/molemod/init/CraftingRecipes.java
+++ src/main/java/com/mod/molemod/init/CraftingRecipes.java
@@ -1,25 +1,23 @@
 package com.mod.molemod.init;
 
-import com.mod.molemod.objects.items.ItemCustomIngredient;
+import com.mod.molemod.MoleMod;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.crafting.IngredientNBT;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import static com.mod.molemod.init.Iteminit.*;
 
 public class CraftingRecipes {
 
-    //private ItemStack test = new ItemStack(LEATHER_SCRAP, 3);
-
     public void initShapedRecipes() {
-
+        GameRegistry.addShapedRecipe(new ResourceLocation("Refined Leather"), null, new ItemStack(LEATHER_REFINED),
+                "   ", "X X", " X ", 'X', LEATHER_SCRAP);
     }
 
     public void initShapelessRecipes() {
-        GameRegistry.addShapelessRecipe(new ResourceLocation("Refined Mole Leather"), null,
-                new ItemStack(LEATHER_REFINED), new ItemCustomIngredient(new ItemStack(LEATHER_SCRAP, 3)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation("Scrapped Leather"), null,
+                new ItemStack(LEATHER_SCRAP, 2), Ingredient.fromStacks(new ItemStack(LEATHER_REFINED)));
     }
 
     public void initSmeltingRecipes() {

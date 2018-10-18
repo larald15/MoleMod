68f3f93128f36766027b3446900125ddd082cfa8
larald15 <larald15@htlkaindorf.at>
2018-10-18 20:09:15 +0200

    Initial Commit

--- src/main/java/com/mod/molemod/objects/tools/ItemCustomWeapon.java
+++ src/main/java/com/mod/molemod/objects/tools/ItemCustomWeapon.java
@@ -1,12 +1,12 @@
 package com.mod.molemod.objects.tools;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemAxe;
 
-public class ItemCustomWeapon extends ItemSword {
+public class ItemCustomWeapon extends ItemAxe {
 
-    public ItemCustomWeapon(String name, ToolMaterial material) {
-        super(material);
+    public ItemCustomWeapon(String name, ToolMaterial material, float damage, float speed) {
+        super(material, damage, speed);
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(CreativeTabs.COMBAT);

426c00be1b9835c0beb5b74070218e0bd402ff29
larald15 <larald15@htlkaindorf.at>
2018-10-18 15:07:19 +0200

    Initial Commit

--- src/main/java/com/mod/molemod/objects/tools/ItemCustomGun.java
+++ src/main/java/com/mod/molemod/objects/tools/ItemCustomGun.java
@@ -3,9 +3,11 @@ package com.mod.molemod.objects.tools;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityLargeFireball;
+import net.minecraft.entity.projectile.EntityTippedArrow;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -30,31 +32,23 @@ public class ItemCustomGun extends Item {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
         ItemStack item = playerIn.getHeldItem(handIn);
         Vec3d aim = playerIn.getLookVec();
-        //EntityLargeFireball bullet = new EntityLargeFireball(worldIn, playerIn, 1, 1, 1);
-        EntityArrow arrow = new EntityArrow(worldIn) {
-            @Override
-            protected ItemStack getArrowStack() {
-                return new ItemStack(BULLET);
-            }
-        };
+        EntityTippedArrow arrow = new EntityTippedArrow(worldIn, playerIn);
 
         Minecraft mc = Minecraft.getMinecraft();
 
-        arrow.arrowShake = 1;
         arrow.setDamage(100);
-
-        //bullet.accelerationX = aim.x * 0.1;
-        //bullet.accelerationY = aim.y * 0.1;
-        //bullet.accelerationZ = aim.z * 0.1;
+        arrow.setPosition(playerIn.posX, playerIn.posY + 0.5, playerIn.posZ);
+        arrow.shoot(aim.x, aim.y, aim.z, 5, 0);
+        arrow.onGround = false;
+        arrow.setFire(3);
 
         if (playerIn.inventory.hasItemStack(new ItemStack(BULLET))) {
-            //worldIn.spawnEntity(bullet);
             worldIn.spawnEntity(arrow);
 
             int slotForBullet = playerIn.inventory.getSlotFor(new ItemStack(BULLET));
             ItemStack itemBullet = playerIn.inventory.getStackInSlot(slotForBullet);
-
             int size = itemBullet.getCount() - 1;
+
             itemBullet.setCount(size);
         } else {
             mc.ingameGUI.displayTitle("You have no ammo!", "", 1, 1, 1);

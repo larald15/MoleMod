623c0cc0fcd73b0011cab4a2eca858282723c8c2
larald15 <larald15@htlkaindorf.at>
2018-10-19 10:37:12 +0200

    Initial Commit

--- src/main/java/com/mod/molemod/objects/tools/ItemCustomWeapon.java
+++ src/main/java/com/mod/molemod/objects/tools/ItemCustomWeapon.java
@@ -1,22 +1,14 @@
 package com.mod.molemod.objects.tools;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.particle.ParticleCrit;
-import net.minecraft.client.particle.ParticleDragonBreath;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldEntitySpawner;
-
-import java.util.Timer;
 
 public class ItemCustomWeapon extends ItemAxe {
 
@@ -27,15 +19,16 @@ public class ItemCustomWeapon extends ItemAxe {
         setCreativeTab(CreativeTabs.COMBAT);
     }
 
+    @Override
     public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack) {
         Vec3d aim = entityLiving.getLook(1);
 
         EntityPlayer playIn = Minecraft.getMinecraft().player;
 
         //get position of the player and adding the aim vector
-        double x = playIn.getPosition().getX() +       aim.x * 4;
-        double y = playIn.getPosition().getY() + 1.5 + aim.y * 4;
-        double z = playIn.getPosition().getZ() +       aim.z * 4;
+        double x = playIn.getPosition().getX() + aim.x * 3;
+        double y = playIn.getPosition().getY() + 1.5 + aim.y * 3;
+        double z = playIn.getPosition().getZ() + aim.z * 3;
 
         //spawning the particle
         World world = entityLiving.getEntityWorld();
@@ -43,7 +36,4 @@ public class ItemCustomWeapon extends ItemAxe {
 
         return false;
     }
-
-    //worked
-
 }
